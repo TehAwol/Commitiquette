1057837ad42a091e002a940f591dbf38aa37f01c
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2023-05-05 14:37:37 +0200

    feat(counter.ts): add double counter

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,20 @@
+let counter = 0;
+let doubleCounter = 0;
+
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
   const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
+}
+
+export function setupDoubleCounter(element: HTMLButtonElement) {
+  const setCounter = (count: number) => {
+    doubleCounter = count;
+    element.innerHTML = `count is ${doubleCounter}`;
+  };
+  element.addEventListener("click", () => setCounter(doubleCounter + 2));
+  setCounter(0);
 }
