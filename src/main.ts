41a9885fd8f10dade3679a35306d0f303f3b0c1b
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2023-05-05 16:02:09 +0200

    feat(main.ts): add triple counter

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import "./style.css";
-import { setupCounter, setupDoubleCounter } from "./counter.ts";
+import {
+  setupCounter,
+  setupDoubleCounter,
+  setupTripleCounter,
+} from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     
     <h1>Commitiquette</h1>
+    <h2>Counter</h2>
     <div class="card">
-      <button id="counter" type="button"></button>
+    <button id="counter" type="button"></button>
     </div>
+    <h2>Double counter</h2>
     <div class="card">
-      <button id="double-counter" type="button"></button>
+    <button id="double-counter" type="button"></button>
+    </div>
+    <h2>Triple counter</h2>
+    <div class="card">
+    <button id="triple-counter" type="button"></button>
     </div>
     <p class="read-the-docs">
       A nonsensical counter project to test Commitizen and Github Wiki.
@@ -20,4 +30,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 setupDoubleCounter(
   document.querySelector<HTMLButtonElement>("#double-counter")!
+);
+setupTripleCounter(
+  document.querySelector<HTMLButtonElement>("#triple-counter")!
 );
