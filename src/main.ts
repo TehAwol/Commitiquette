20c8e1685d05f61166c81ab4b726a6cde11bbcf6
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2023-05-05 15:28:43 +0200

    refactor(main.ts): remove unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 import { setupCounter, setupDoubleCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
